276cadfc905f1819c131fd8b52f51e12211bc129
Rachel Sison <rsison87@gmail.com>
2015-02-23 13:59:19 -0800

    fixed typo in filename and completed board square validation

--- sudokuBoardValidator.js
+++ sudokuBoardValidator.js
@@ -1,7 +1,8 @@
 var board;
 var boardCoordinates = [];
 var oneThroughNine;
-var currentColumn
+var currentColumn;
+var boardSquares = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]};
 
 var checkRows = function(){
   //if numbers in all rows 1-9 and unique return true
@@ -37,8 +38,41 @@ var checkColumns = function(){
   return true;
 }
 
+var checkList = function(list){
+  oneThroughNine = {1:true, 2:true, 3:true, 4:true, 5:true, 6:true, 7:true, 8:true, 9:true};
+  for(var i=0; i < 9; i++){
+    if(oneThroughNine[list[i]]){
+      delete oneThroughNine[list[i]];
+    }
+  }
+  if(Object.keys(oneThroughNine).length > 0){
+    return false;
+  }
+  return true;
+}
+
 var checkSquares = function(){
   //if numbers in all 3x3 squares 1-9 and unique return true
+  var currentSquare;
+  //this loop goes through square columns left to right
+  for(var i=0; i < 9; i+=3){
+    //this loop goes through square rows top to bottom within current square column of above loop
+    for(var j=0; j < 9; j+=3){
+      currentSquare = [];
+      oneThroughNine = {1:true, 2:true, 3:true, 4:true, 5:true, 6:true, 7:true, 8:true, 9:true};
+      //this loop starts at loop start in j to create row (j -> j+3)
+      for(var k=j; k < j+3; k++){
+        //iterates through
+        for(var l=i; l < i+3; i++){
+          currentSquare.push(board[k][l]);
+        }
+      }
+      if(!checkList(currentSquare)){
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 
